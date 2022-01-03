589c5281a1a997851b11cc50867ccae16c01b035
xiaohuajun <18061741650@163.com>
2022-01-03 15:17:43 +0800

    cas学习

---
+++ src/main/java/com/xiaohuajun/wonderful/servce/DemoService.java
@@ -0,0 +1,26 @@
+package com.xiaohuajun.wonderful.servce;
+
+/**
+ * @author xiaohuajun
+ * @version 1.0
+ * @date 2022/1/2 下午4:48
+ * @description DemoService
+ */
+public class DemoService {
+
+
+  public static void main(String[] args) {
+
+    int a = 5;
+    int b = 8;
+    //如果 a < b:余数是最小-> a， 如果在
+    int c = a % b;
+    //向下取整数
+    int d = 5 / 2;
+    int k = 2 / 5;
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(k);
+  }
+
+}
