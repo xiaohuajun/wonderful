5cb3fb45d5c1308eb3fe3ddb1fa038ee7d82ac62
xiaohuajun <18061741650@163.com>
2023-05-02 17:28:59 +0800

    增加启动app的注释

--- src/main/java/com/xiaohuajun/wonderful/WonderfulApplication.java
+++ src/main/java/com/xiaohuajun/wonderful/WonderfulApplication.java
@@ -3,6 +3,9 @@ package com.xiaohuajun.wonderful;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Danny
+ */
 @SpringBootApplication
 public class WonderfulApplication {
 
