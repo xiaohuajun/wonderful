f0ec6dc1ae02e3b358a96b1bf6bbd100e841cc90
xiaohuajun <18061741650@163.com>
2023-05-02 13:13:36 +0800

    项目新建

---
+++ src/main/java/com/xiaohuajun/wonderful/WonderfulApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohuajun.wonderful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WonderfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaohuajun/wonderful/WonderfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohuajun.wonderful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
